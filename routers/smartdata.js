a15ccae745765e38c3d19657f662f80bd2beb3e5
mayank@10medz.com <mayank@10medz.com>
2025-05-18 17:50:07 +0530

    authentication added in fetchSmartData

--- routers/smartdata.js
+++ routers/smartdata.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { fetchSmartData } from '../controller/smartdata.js';
 import { prescription } from '../middleware/multer.js';
+import { isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router();
 
 // Route for processing prescription images
-router.post('/fetchsmartdata', prescription, fetchSmartData);
+router.post('/fetchsmartdata', isAuthenticated, prescription, fetchSmartData);
 
 export default router; 
